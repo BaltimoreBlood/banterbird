0b03bca0996405d0d4fb48cb6624061560a89136
BaltimoreBlood <keegan.cantrell@ascte.org>
2025-05-02 11:00:07 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -52,8 +52,8 @@ setInterval(async () =>{
     try {
         const response = await fetch("/api/posts");
         const posts = await response.json();
-        posts.forEach((post) => renderPost(post));
         document.getElementById("feed").innerHTML = "";
+        posts.forEach((post) => renderPost(post));
     } catch (error) {
         console.error("Error fetching posts:", error);
     }
